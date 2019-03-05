1fcc27a2ebf3ee50e34bc78a8a12c82d995b9546
Felipe Parreira <felipeparreirafb@gmail.com>
2019-03-05 10:47:11 -0800

    Implement score table over games

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -7,19 +7,24 @@ class App extends Component {
     super(props);
 
     this.state = {
-      guessesLeft: 7,
+      guessesLeft: 0,
       currentWord: {},
-      wrongChars: {},
+      wrongChars: new Set(),
       displayedChars: [],
       blockInput: false,
-      correctGuesses: 0,
-      score: {
-        wins: 0,
-        losses: 0
-      }
+      wins: 0,
+      losses: 0,
+      level: 'easy',
+      lengths: {
+        easy: { maxLength: 5 },
+        medium: { maxLength: 9 },
+        hard: { maxLength: 15 }
+      },
+      scores: []
     };
 
     this.handleSubmitCharClick = this.handleSubmitCharClick.bind(this);
+    this.handleLevelChange = this.handleLevelChange.bind(this);
     this.restartGame = this.restartGame.bind(this);
   }
 
@@ -28,8 +33,9 @@ class App extends Component {
   }
 
   restartGame() {
-    const randomWord = randomWords().toUpperCase();
-    let currentWord = {};
+    const lengthsPerLevel = this.state.lengths[this.state.level];
+    const randomWord = randomWords({ exactly: 1, ...lengthsPerLevel })[0].toUpperCase();
+    const currentWord = { word: randomWord };
 
     for (let i = 0; i < randomWord.length; i++) {
       const char = randomWord[i];
@@ -38,70 +44,137 @@ class App extends Component {
     }
 
     this.setState({
-      guessesLeft: 7,
+      guessesLeft: Math.ceil(0.6 * randomWord.length),
       currentWord,
       displayedChars: new Array(randomWord.length).fill('__'),
-      wrongChars: {},
-      blockInput: false,
-      correctGuesses: 0
+      wrongChars: new Set(),
+      blockInput: false
     });
   }
 
   handleSubmitCharClick(event, char) {
     event.preventDefault();
-    let { currentWord, guessesLeft, wrongChars, displayedChars, correctGuesses } = this.state;
+    let { blockInput, scores, currentWord, guessesLeft, wrongChars, displayedChars, wins, losses } = this.state;
+
+    // exit the function if the char has already been tried
+    if (char !== '' && char !== '__' && (wrongChars.has(char) || displayedChars.includes(char))) return;
 
     if (currentWord[char]) {
-      correctGuesses++;
-      if (correctGuesses === currentWord.length) {
-        this.setState({
-          blockInput: true
-        });
-      }
 
       displayedChars = displayedChars.slice();
       for (let index of currentWord[char]) {
         displayedChars[index] = char;
       }
 
+      currentWord = { ...currentWord };
       delete currentWord[char];
 
-      this.setState({
-        displayedChars,
-        correctGuesses
-      });
+      // check if all chars have been correctly guessed 
+      // (the only property that remains is the word itself)
+      if (Object.keys(currentWord).length === 1) {
+        wins++;
+
+        scores = scores.concat({ 
+          wrongGuesses: Array.from(wrongChars), 
+          word: currentWord.word,
+          won: true 
+        });
+
+        this.setState({
+          scores,
+          wins
+        });
+
+        window.alert(`YOU WON! The word was ${currentWord.word}`);
+        this.restartGame();
+
+      } else {
+        this.setState({
+          currentWord,
+          displayedChars
+        });
+      }
 
     } else {
-      guessesLeft && guessesLeft--;
-
-      if (!guessesLeft) {
-        window.alert('GAME OVER!');
+      if (guessesLeft === 1) {
+        losses++;
+        scores = scores.concat({ 
+          wrongGuesses: Array.from(wrongChars), 
+          word: currentWord.word,
+          won: false
+        });
       }
+
+      guessesLeft && guessesLeft--;
+      wrongChars = new Set(wrongChars);
+      wrongChars.add(char);
 
       this.setState({
         guessesLeft,
-        wrongChars: {...wrongChars, [char]: true}
+        wrongChars,
+        scores,
+        losses
       });
+
+      if (!guessesLeft) {
+        window.alert(`GAME OVER! The word was ${currentWord.word}`);
+        this.restartGame();
+      }
       
     }
+
+    console.log('scores inside', scores);
+  }
+
+  handleLevelChange(event) {
+    event.preventDefault();
+    this.setState({
+      level: event.target.value
+    });
   }
 
   render() {
-  const { currentWord, guessesLeft, wrongChars, displayedChars, score } = this.state;
-  console.log('word', currentWord);
-
+  const { scores, currentWord, guessesLeft, wrongChars, displayedChars, wins, losses } = this.state;
+  console.log('currentWord', currentWord);
+  
   return (
       <div>
-        <div>Wins: {score.wins} Losses: {score.losses}</div>
+        <div>Played: {wins + losses} | Wins: {wins} | Losses: {losses}</div>
         <div>Guesses Left: {guessesLeft}</div>
         <div>Wrong characters: 
-          {Object.keys(wrongChars).map((char, index) => (<span key={char + index}>{char} </span>))}
+          {Array.from(wrongChars).map((char, index) => (<span key={char + index}>{char} </span>))}
         </div>
         <div>
           {displayedChars.map((char, index) => <span key={char + index}>{char} </span>)}
         </div>
         <CharacterInput handleSubmitCharClick={this.handleSubmitCharClick}/>
+        <select onChange={this.handleLevelChange}>
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
         <button onClick={(e) => this.restartGame()}>Restart game!</button>
+        <div>
+          <h3>Score Table</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Wrong Guesses</th>
+                <th>Word</th> 
+                <th>Result</th>
+              </tr>
+            </thead>
+            <tbody>
+              { scores.map(score => (
+                <tr key={score.word}>
+                  <td>{score.wrongGuesses.length}</td>
+                  <td>{score.word}</td> 
+                  <td>{score.won ? 'won' : 'lost'}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
